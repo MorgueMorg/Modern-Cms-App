1f52ad0a98d41767134e0e993ce8428eff44b504
Dzhanaliev Aktan <dzhanaliev.aktan@gmail.com>
2022-07-30 20:13:38 +0600

    added env

--- services/index.js
+++ services/index.js
@@ -1,5 +1,7 @@
 import { request, gql } from "graphql-request";
 
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT
+
 export const getPosts = async () => {
   const query = gql`
     query MyQuery {
