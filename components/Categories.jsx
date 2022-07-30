5665725d6892f46a7e7003f009e0d48961da52ef
Dzhanaliev Aktan <dzhanaliev.aktan@gmail.com>
2022-07-30 21:51:34 +0600

    finished categories

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+
+import { getCategories } from "../services";
 
 const Categories = () => {
-  return (
-    <div>Categories</div>
-  )
-}
+  const [categories, setCategories] = useState([]);
 
-export default Categories
+  useEffect(() => {
+    getCategories().then((newCategories) => setCategories(newCategories));
+  }, []);
+
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+        Categories
+      </h3>
+      {categories.map((category) => (
+        <Link key ={category.slug} href={`/category/${category.slug}`}>
+          <span className="cursor-pointer block pb-3 mb-3">
+            {category.name}
+          </span>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Categories;
--- services/index.js
+++ services/index.js
@@ -79,3 +79,17 @@ export const getSimilarPosts = async() => {
 
   return result.posts;
 }
+
+export const getCategories = async() => {
+  const query = gql`
+    query GetCategories {
+      categories {
+        name
+        slug
+      }
+    }
+  `
+  const result = await request(graphqlAPI, query);
+
+  return result.categories;
+}
