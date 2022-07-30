1b31766346a81fdd41fa96b84b44716bd649a210
Dzhanaliev Aktan <dzhanaliev.aktan@gmail.com>
2022-07-30 20:36:11 +0600

    Update PostCard and get request from Graphql

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import moment from 'moment'
-import Link from 'next/link'
+import React from "react";
+import moment from "moment";
+import Link from "next/link";
 
 const PostCard = ({ post }) => {
+  console.log(post);
   return (
-    <div>
-        {post.title}
-        {post.excerpt}
+    <div className="bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8">
+      <div className="relative overflow-hidden shadow-md pb-80 mb-6">
+        <img
+          src={post.featuredImage.url}
+          alt=""
+          className="object-top absolute h-80 w-full object-cover  shadow-lg rounded-t-lg lg:rounded-lg"
+        />  
+      </div>
+      <h1></h1>
     </div>
-  )
-}
+  );
+};
 
-export default PostCard
+export default PostCard;
--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ posts }) {
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post, index) => (
-            <PostCard key={post.title} post={post} />
+            <PostCard key={post.title} post={post.node} />
           ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
