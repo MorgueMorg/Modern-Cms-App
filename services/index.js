1abcedfb24f78a5f09dec27a40095b580d340673
Dzhanaliev Aktan <dzhanaliev.aktan@gmail.com>
2022-07-30 20:10:24 +0600

    added services

---
+++ services/index.js
@@ -0,0 +1,34 @@
+import { request, gql } from "graphql-request";
+
+export const getPosts = async () => {
+  const query = gql`
+    query MyQuery {
+      postsConnection {
+        edges {
+          node {
+            author {
+              bio
+              name
+              id
+              photo {
+                url
+              }
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
+          }
+        }
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query)
+};
