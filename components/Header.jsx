c497983d077acd00bbb442399ee52eba1963ff25
Dzhanaliev Aktan <dzhanaliev.aktan@gmail.com>
2022-07-30 21:56:43 +0600

    fixed header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,15 @@
-import React, { useContext } from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 
-const categories = [
-  { name: "React", slug: "react" },
-  { name: "Web Development", slug: "web-dev" },
-];
+import { getCategories } from "../services";
 
 const Header = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((newCategories) => setCategories(newCategories));
+  }, []);
+
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="border-b w-full inline-block border-blue-400 py-8">
