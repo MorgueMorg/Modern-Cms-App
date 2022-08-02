07e65660904fa602544b50749755d1838be83e48
Dzhanaliev Aktan <dzhanaliev.aktan@gmail.com>
2022-08-02 15:11:18 +0600

    added util file

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Image from 'next/image';
 
+import { grpahCMSImageLoader } from '../util';
+
 const Author = ({ author }) => (
   <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20">
     <div className="absolute left-0 right-0 -top-14">
       <Image
         unoptimized
+        loader={grpahCMSImageLoader}
         alt={author.name}
         height="100px"
         width="100px"
