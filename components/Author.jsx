908623e58db394cdab40990f4333a5759e79e619
Dzhanaliev Aktan <dzhanaliev.aktan@gmail.com>
2022-08-02 15:09:26 +0600

    added author content

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 
-const Author = () => {
-  return (
-    <div>Author</div>
-  )
-}
+const Author = ({ author }) => (
+  <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20">
+    <div className="absolute left-0 right-0 -top-14">
+      <Image
+        unoptimized
+        alt={author.name}
+        height="100px"
+        width="100px"
+        className="align-middle rounded-full"
+        src={author.photo.url}
+      />
+    </div>
+    <h3 className="text-white mt-4 mb-4 text-xl font-bold">{author.name}</h3>
+    <p className="text-white text-ls">{author.bio}</p>
+  </div>
+);
 
-export default Author
+export default Author;
