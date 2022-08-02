ede79447e31847889077a446cae6559efc163b00
Dzhanaliev Aktan <dzhanaliev.aktan@gmail.com>
2022-08-02 16:02:43 +0600

    added featured posts and carousel

--- components/index.js
+++ components/index.js
@@ -1,9 +1,9 @@
-export { default as PostCard } from './PostCard'
-export { default as Categories } from './Categories';
-export { default as PostWidget } from './PostWidget';
-export { default as Header } from './Header'
+export { default as PostCard } from './PostCard';
+export { default as PostDetail } from './PostDetail';
 export { default as Layout } from './Layout';
+export { default as Categories } from './Categories';
 export { default as Author } from './Author';
-export { default as CommentsForm } from './CommentsForm';
+export { default as PostWidget } from './PostWidget';
+export { default as FeaturedPostCard } from './FeaturedPostCard';
 export { default as Comments } from './Comments';
-export { default as PostDetail} from './PostDetail';
+export { default as CommentsForm } from './CommentsForm';
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { PostCard, Categories, PostWidget } from "../components";
 import { getPosts } from "../services";
+import { FeaturedPosts } from '../sections/index';
 
 export default function Home({ posts }) {
   return (
@@ -9,6 +10,7 @@ export default function Home({ posts }) {
         <title>ModernCms</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post, index) => (
