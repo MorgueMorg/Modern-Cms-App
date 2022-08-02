fb26f5e063b9168eed537e48718759df96d39101
Dzhanaliev Aktan <dzhanaliev.aktan@gmail.com>
2022-08-02 16:05:32 +0600

    added loader and new slug

---
+++ pages/category/[slug].js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+
+import { getCategories, getCategoryPost } from '../../services';
+import { PostCard, Categories, Loader } from '../../components';
+
+const CategoryPost = ({ posts }) => {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <Loader />;
+  }
+
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="col-span-1 lg:col-span-8">
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
+          ))}
+        </div>
+        <div className="col-span-1 lg:col-span-4">
+          <div className="relative lg:sticky top-8">
+            <Categories />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default CategoryPost;
+
+export async function getStaticProps({ params }) {
+  const posts = await getCategoryPost(params.slug);
+
+  return {
+    props: { posts },
+  };
+}
+
+export async function getStaticPaths() {
+  const categories = await getCategories();
+  return {
+    paths: categories.map(({ slug }) => ({ params: { slug } })),
+    fallback: true,
+  };
+}
